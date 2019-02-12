6ad57ef4a4dd265e554443ad32bd1c8041fe515d
Nicholas Christopoulos <nereus@freemail.gr>
2019-02-12 17:31:20 +0200

    quick and dirty fix

---
+++ .Grief/grief-rc.cr
@@ -0,0 +1,19 @@
+/* -*- mode: cr; indent-width: 4; -*- */
+
+void main(void)
+{
+}
+
+void _startup_complete(int mode)
+{
+	assign_to_key("<Ctrl-C>", "copy");
+	assign_to_key("<Ctrl-X>", "cut");
+	assign_to_key("<Ctrl-V>", "paste");
+	
+	assign_to_key("<Ctrl-Up>", inq_assignment("<PgUp>"));
+	assign_to_key("<Ctrl-Down>", inq_assignment("<PgDn>"));
+	
+	assign_to_key("<Ctrl-Left>", inq_assignment("<Home>"));
+	assign_to_key("<Ctrl-Right>", inq_assignment("<End>"));
+}
+
