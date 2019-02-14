93b113e02a92c3abd02810f3b955f4d564642f86
Nicholas Christopoulos <nereus@freemail.gr>
2019-02-14 11:09:08 +0200

    quick and dirty fix

--- .grief-local/grief-rc.cr
+++ .grief-local/grief-rc.cr
@@ -16,6 +16,14 @@ void _startup_complete(int mode)
 	assign_to_key("<Ctrl-Left>", inq_assignment("<Home>"));
 	assign_to_key("<Ctrl-Right>", inq_assignment("<End>"));
 
-	assign_to_key("<Alt-F10>", "make");
+/* Borland's BCB6 keys */
+/*	assign_to_key("<Ctrl-F1>", "topic help"); */
+/*	assign_to_key("<Ctrl-F2>", "program reset"); */
+/*	assign_to_key("<Alt-F2>", "window zoom"); */
+	assign_to_key("<Ctrl-F9>", "make");
+/*	assign_to_key("<Alt-F7>", "previous error"); */
+/*	assign_to_key("<Alt-F8>", "next error"); */
+/*	assign_to_key("<F9>", "run"); Borland's default */
+/*	assign_to_key("<Ctrl-F10>", "run"); */
 }
 
