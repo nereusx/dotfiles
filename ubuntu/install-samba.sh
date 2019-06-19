9c063ed59aa242190b972a6d8c83d36a39323be1
Nicholas Christopoulos <nereus@freemail.gr>
2019-06-19 22:54:45 +0300

    xubuntu installation scripts

---
+++ ubuntu/install-samba.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+app=./ubuntu-pkg
+if [ ! -d /srv/public ]; then
+	mkdir /srv/public
+	chown -R nobody:nogroup /srv/public
+	chmod -R ugo+rswX /srv/public
+fi
+$app -i smbclient samba smb4k fusesmb
+cp etc/samba/* /etc/samba/
+$app -e smbd
