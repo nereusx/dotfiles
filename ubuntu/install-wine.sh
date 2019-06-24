8859e1761e331663530822aa4e29bb2e4ac56d35
Nicholas Christopoulos <nereus@freemail.gr>
2019-06-24 16:39:14 +0300

    ubuntu wine updated

--- ubuntu/install-wine.sh
+++ ubuntu/install-wine.sh
@@ -4,4 +4,7 @@ sudo apt-get update
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ disco main'
 wget -nc https://dl.winehq.org/wine-builds/winehq.key && sudo apt-key add winehq.key
 sudo apt-get update
-sudo apt install --install-recommends winehq-stable
+sudo apt remove wine
+sudo apt install --install-recommends winehq-staging
+ln -svf /opt/wine-staging/bin/wine /usr/bin/wine
+ln -svf /opt/wine-staging/bin/wineserver /usr/bin/wineserver
