3e8db232238c92ece9a4cc11bedb31bd9d251d08
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-08 02:12:19 +0300

    quick and dirty fix

--- ubuntu/install-arpd.sh
+++ ubuntu/install-arpd.sh
@@ -10,4 +10,8 @@ if [ ! -d /var/lib/arpd ]; then
 	mkdir /var/lib/arpd
 fi
 
+### add it to /etc/rc.local
 grep -w arpd /etc/rc.local > /dev/null || echo "/usr/sbin/arpd" >> /etc/rc.local
+
+### run it
+[ pgrep -u root -x arpd > /dev/null ] || arpd
