a37864882a28540cd48b31d5001d9722a16b2e0e
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-08 20:18:56 +0300

    quick and dirty fix

--- ubuntu/pulse-user-fix.sh
+++ ubuntu/pulse-user-fix.sh
@@ -4,15 +4,15 @@
 #
 
 logger -t PAUSER "Check PulseAudio of $USER ($uid)"
-pgrep -u $uid pulseaudio
-if ( $? == 0 ) then
-	logger -t PAUSER "Found!"
-	( sleep 10; \
-		/usr/local/bin/pulseaudio-stop.sh; \
-		sleep 5; \
-		pactl set-default-sink 0; \
-		notify-send "PulseAudio Patch Applied" ) &
-	logger -t PAUSER "It will removed in 15s"
-else
-	logger -t PAUSER "No found"
-endif
+while ( 1 )
+	pgrep -u $uid pulseaudio
+	if ( $? == 0 ) then
+		logger -t PAUSER "Found!"
+		/usr/local/bin/pulseaudio-stop.sh
+		pactl set-default-sink 0
+		logger -t PAUSER "removed?"
+	else
+		logger -t PAUSER "No found"
+		break
+	endif
+end
