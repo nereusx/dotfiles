53fd03235dc972d7d86ba2501afc7e0d6f21d0e0
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-08 20:47:45 +0300

    quick and dirty fix

--- ubuntu/pulse-user-fix.sh
+++ ubuntu/pulse-user-fix.sh
@@ -1,19 +1,32 @@
 #!/bin/tcsh
 
 logger -t PAUSER "Check PulseAudio of $USER ($uid)"
+
+logger -t PAUSER "Waiting ... the monster"
+while ( 1 )
+	if ( { pgrep -u $uid pulseaudio } ) then
+		break
+	endif
+	sleep 5
+	logger -t PAUSER "Waiting ..."
+end
+
+#
 while ( 1 )
 	if ( { pgrep -u $uid pulseaudio } ) then
 		logger -t PAUSER "Found!"
 		systemctl --user stop pulseaudio.socket
-		sleep 2
+		sleep $delay
 		systemctl --user stop pulseaudio.service
-		sleep 8
+		sleep 5
 		logger -t PAUSER "removed?"
 	else
 		logger -t PAUSER "The monster is dead"
+		notify-send -t PAUSER "The monster is dead"
 		break
 	endif
-	sleep 2
+	sleep 5
 end
-pactl set-default-sink 0
 logger -t PAUSER "Change the default sound card"
+pactl set-default-sink 0
+echo "* done *"
