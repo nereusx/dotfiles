447f0ba7d21fa4cd731f7740ad0b8fb26c645bbf
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-08 03:48:24 +0300

    quick and dirty fix

--- ubuntu/install-arpd.sh
+++ ubuntu/install-arpd.sh
@@ -14,4 +14,6 @@ fi
 grep -w arpd /etc/rc.local > /dev/null || echo "/usr/sbin/arpd" >> /etc/rc.local
 
 ### run it
-[ pgrep -u root -x arpd > /dev/null ] || arpd
+pgrep -u root -x arpd > /dev/null || arpd
+
+echo "* get report: arpd -l *"
--- ubuntu/install-viber.sh
+++ ubuntu/install-viber.sh
@@ -2,8 +2,8 @@
 apt install flatpak
 [ ! -d ~/.bin ] && mkdir ~/.bin
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak update
-flatpak install com.viber.Viber
+flatpak -y update
+flatpak -y install com.viber.Viber
 cat << EOF > ~/.bin/viber
 #!/bin/sh
 exec flatpak run com.viber.Viber
