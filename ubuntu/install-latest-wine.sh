7e23ee1d33244c35028868dccbf4c9cda49d9f7d
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-12 19:45:46 +0300

    quick and dirty fix

--- ubuntu/install-latest-wine.sh
+++ ubuntu/install-latest-wine.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-sudo apt-get upgrade
-sudo apt-get update
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ disco main'
+apt-get upgrade
+apt-get update
+apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ disco main'
 wget -nc https://dl.winehq.org/wine-builds/winehq.key && sudo apt-key add winehq.key
-sudo apt-get update
-sudo apt remove wine
-sudo apt install --install-recommends winehq-staging
+apt-get update
+apt remove wine
+apt install --install-recommends winehq-staging
 ln -svf /opt/wine-staging/bin/wine /usr/bin/wine
 ln -svf /opt/wine-staging/bin/wineserver /usr/bin/wineserver
