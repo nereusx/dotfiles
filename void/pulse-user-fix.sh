f3871d847bd954df445d7edeb41e66e0531743e5
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-17 19:08:18 +0300

    quick and dirty fix

---
+++ void/pulse-user-fix.sh
@@ -0,0 +1,45 @@
+#!/bin/tcsh
+#
+#	This script kills the local user's pulseaudio
+#	Use it in X session startup if you use pulseaudio daemon
+#
+
+# play this sound on startup
+set playfile = /usr/share/sounds/ubuntu/stereo/desktop-login.ogg
+
+#
+logger -t PAUSER "Check PulseAudio of $USER ($uid)"
+
+logger -t PAUSER "Waiting ... the monster"
+echo "Waiting..."
+while ( 1 )
+	if ( { pgrep -u $uid pulseaudio } ) then
+		break
+	endif
+	logger -t PAUSER "Waiting ..."
+	sleep 1
+end
+
+#
+while ( 1 )
+	if ( { pgrep -u $uid pulseaudio } ) then
+		logger -t PAUSER "Found!"
+		killall -u $USER pulseaudio
+		sleep 1
+		logger -t PAUSER "removed?"
+	else
+		logger -t PAUSER "The monster is dead"
+		break
+	endif
+	sleep 1
+end
+
+logger -t PAUSER "Change the default sound card"
+pactl set-default-sink 0
+
+sleep 1
+
+if ( $?playfile ) then
+	paplay $playfile &
+endif
+notify-send "USER's PulseAudio Killed"
