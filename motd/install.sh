21a0d541ea98ad3b28f5f6828443d1eb2a7163c2
Nicholas Christopoulos <nereus@freemail.gr>
2019-07-18 06:20:26 +0300

    quick and dirty fix

--- motd/install.sh
+++ motd/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DIR=$PWD
-ln -svf $DIR/sbin/motd-create         /usr/local/sbin/motd-create
-ln -svf $DIR/sbin/motd-add            /usr/local/sbin/motd-add
-ln -svf $DIR/daily/motd-daily-update  /etc/cron.daily/motd-daily-update
+cp $DIR/sbin/motd-create         /usr/local/sbin/motd-create
+cp $DIR/sbin/motd-add            /usr/local/sbin/motd-add
+cp $DIR/daily/motd-daily-update  /etc/cron.daily/motd-daily-update
