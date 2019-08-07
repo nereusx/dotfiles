1a50ab3840912936476d210403174af04e44cc09
Nicholas Christopoulos <nereus@freemail.gr>
2019-08-07 18:36:07 +0300

    quick and dirty fix

--- void-linux/install-viber.sh
+++ void-linux/install-viber.sh
@@ -14,3 +14,34 @@ chmod +x ~/.bin/viber
 # exec env QT_SCALE_FACTOR=0.5 flatpak run com.viber.Viber
 
 
+# === without flatpak ===
+#
+### install openssl-1.0
+#
+# wget https://www.openssl.org/source/openssl-1.0.2s.tar.gz
+# tar xvfz openssl-1.0.2s.tar.gz
+# cd openssl-1.0.2s
+# ./config
+# make
+# make install
+# cd -
+#
+### install alien
+#
+# cd missing-packages/alien-8.95
+# ./Makefile.PL
+# make
+# make install
+# cd -
+#
+### install viber
+#
+# wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+# alien -t --scripts viber.deb
+# tar xvfz viber[TAB] -C /
+#
+### run it
+#
+# /opt/viber/Viber &
+#
+
