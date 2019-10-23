f3eeb95c9950c7cdd327966002a21189685734f3
Nicholas Christopoulos <nereus@freemail.gr>
2019-10-23 20:43:32 +0300

    quick and dirty fix

--- local-bin/install.sh
+++ local-bin/install.sh
@@ -1,28 +1,17 @@
-#!/bin/tcsh -f
+#!/bin/sh
 
-set dest=/usr/local/bin
-if ( ! -d $dest ) then
+dest=/usr/local/bin
+if [ ! -d $dest ]; then
 	mkdir -p $dest
 	chown root:root $dest
-	chmod 0750 $dest
-endif
-if ( "$1" == "-s" ) then
-	foreach f ( * )
-		if ( $f:e == "" ) then
-			if ( -e "$dest/$f" ) then
-				rm "$dest/$f"
-			endif
-			ln -svf "$PWD/$f" "$dest/$f"
-		endif
-	end
-else
-	foreach f ( * )
-		if ( $f:e == "" ) then
-			if ( -l "$dest/$f" ) then
-				rm "$dest/$f"
-			endif
-			cp $f "$dest/$f"
-		endif
-	end
-endif
+	chmod 0755 $dest
+fi
+for f in *; do
+	fb="${f%%.*}"
+	if [ -x "$fb" ]; then
+		echo $f
+		cp $f "$dest/$f"
+	fi
+done
+
 
--- local-sbin/install.sh
+++ local-sbin/install.sh
@@ -1,28 +1,17 @@
-#!/bin/tcsh -f
+#!/bin/sh
 
-set dest=/usr/local/sbin
-if ( ! -d $dest ) then
+dest=/usr/local/sbin
+if [ ! -d $dest ]; then
 	mkdir -p $dest
 	chown root:root $dest
 	chmod 0750 $dest
-endif
-if ( "$1" == "-s" ) then
-	foreach f ( * )
-		if ( $f:e == "" ) then
-			if ( -e "$dest/$f" ) then
-				rm "$dest/$f"
-			endif
-			ln -svf "$PWD/$f" "$dest/$f"
-		endif
-	end
-else
-	foreach f ( * )
-		if ( $f:e == "" ) then
-			if ( -l "$dest/$f" ) then
-				rm "$dest/$f"
-			endif
-			cp $f "$dest/$f"
-		endif
-	end
-endif
+fi
+for f in *; do
+	fb="${f%%.*}"
+	if [ -x "$fb" ]; then
+		echo $f
+		cp $f "$dest/$f"
+	fi
+done
+
 
