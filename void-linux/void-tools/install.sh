4df8bccb420c60b3d70ee5c9e308bfedea10f4b0
Nicholas Christopoulos <nereus@freemail.gr>
2019-10-31 15:32:26 +0200

    quick and dirty fix

---
+++ void-linux/void-tools/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+dest=/usr/local/bin
+if [ ! -d $dest ]; then
+	mkdir -p $dest
+	chown root:root $dest
+	chmod 0755 $dest
+fi
+for f in *; do
+	bas=${f%.*}
+	ext=${f#"$bas"}
+	if [ -z "$ext" -a -x "$bas" ]; then
+		echo $f
+		cp $f "$dest/$f"
+	fi
+done
+
+
