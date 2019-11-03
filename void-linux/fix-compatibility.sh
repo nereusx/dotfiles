3b311505f7e039814f4a2b135a581a3c94063135
Nicholas Christopoulos <nereus@freemail.gr>
2019-11-03 05:29:41 +0200

    quick and dirty fix

--- void-linux/fix-compatibility.sh
+++ void-linux/fix-compatibility.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+xbps-alternatives -s net-tools
 xbps-alternatives -s inetutils-hostname
+xbps-alternatives -s inetutils-ifconfig
+xbps-alternatives -s inetutils-syslog
+xbps-alternatives -s inetutils-ping
+xbps-alternatives -s inetutils-traceroute
+xbps-alternatives -s ksh
+
+
