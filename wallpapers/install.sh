a0c6bf31f23d4ccae933f4266ae2e83cc344bb42
Nicholas Christopoulos <nereus@freemail.gr>
2019-11-22 22:40:43 +0200

    fix install wallpapers

--- wallpapers/install.sh
+++ wallpapers/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-list=(/usr/share/wallpapers /usr/share/backdrops /usr/share/backgrounds /usr/local/share/backgrounds)
-for e in ${list[@]}; do
+list="/usr/share/wallpapers /usr/share/backdrops /usr/share/backgrounds /usr/local/share/backgrounds"
+for e in $list; do
 	if [ -d $e ]; then
 		cp *.png $e
 		exit 0
