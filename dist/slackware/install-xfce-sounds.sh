cb4a94bde27ef0dc23a5003ab47f4f83f66aa5b0
Nicholas Christopoulos <nereus@freemail.gr>
2019-12-06 04:57:04 +0200

    quick and dirty fix

---
+++ dist/slackware/install-xfce-sounds.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# there are no sound-themes
+slackpkg install libcanberra-gtk xfce4-settings xsettingsd xfconf libcanberra-utils
+
+cp etc/profile.d/gtk-sound-module.sh /etc/profile.d/
+xfconf-query -c xsettings -p /Net/SoundThemeName -s ubuntu
+
+# added to sounds/sound-themes
+## unzip sound theme
+#unzip ubuntu-sound-theme.zip -d /
+
+echo "you need to reboot to enable XFCE4 sounds"
