a42a9ca9d2bce9a8a1517d67d768f7aa8840ac18
Nicholas Christopoulos <nereus@freemail.gr>
2020-04-30 02:57:10 +0300

    quick and dirty fix

---
+++ dist/void/ignore-dbus.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# VOID - ignore dbus
+
+echo "ignorepkg=dbus" >> /etc/xbps.d/99-ignore.conf
+echo "ignorepkg=dbus-x11" >> /etc/xbps.d/99-ignore.conf
+echo "ignorepkg=elogind" >> /etc/xbps.d/99-ignore.conf
+echo "ignorepkg=polkit" >> /etc/xbps.d/99-ignore.conf
+				
+				
