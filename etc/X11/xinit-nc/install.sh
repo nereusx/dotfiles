5e3a4b3fd01d9d093cbca3cff5a429b6cf151003
Nicholas Christopoulos <nereus@freemail.gr>
2020-09-24 23:56:46 +0300

    quick and dirty fix

--- etc/X11/xinit-nc/install.sh
+++ etc/X11/xinit-nc/install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-cp -rf ../xinit-nc /etc/X11/
+xdir=$(dirname $PWD)
+if [ ! "$xdir" = "/etc/X11" ]; then
+	echo "copy configs to /etc/X11/xinit-nc"
+	cp -rf ../xinit-nc /etc/X11/
+fi
+echo "copy xwmlist and xwmconfig to /usr/local/bin"
 cp xwmlist   /usr/local/bin
 cp xwmconfig /usr/local/bin
