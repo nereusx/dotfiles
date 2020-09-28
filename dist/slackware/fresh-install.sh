8de4ca350dddd552b5a5e0d10d46b0c609ea7d1a
Nicholas Christopoulos <nereus@freemail.gr>
2020-09-28 09:30:29 +0300

    quick and dirty fix

--- dist/slackware/fresh-install.sh
+++ dist/slackware/fresh-install.sh
@@ -5,7 +5,7 @@ cp tools/* /usr/sbin/
 
 ./install-slackpkg-plus.sh
 ./install-sbopkg.sh
-./install-my-unix-utils.sh
+./install-ndc-unix-utils.sh
 ./install-xclip.sh
 ./install-pick-ndc-version.sh
 ./install-fzy.sh
