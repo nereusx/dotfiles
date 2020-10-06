1613f920f9565db3a2272411514a48ec412785fb
Nicholas Christopoulos <nereus@freemail.gr>
2020-10-06 23:20:02 +0300

    quick and dirty fix

--- fonts/install-iosevka.sh
+++ fonts/install-iosevka.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
+VER=3.6.3
 set -e
 . ./TTFDIR
 #
-echo "+ download..."
-wget https://github.com/be5invis/Iosevka/releases/download/v2.3.2/02-iosevka-term-2.3.2.zip
-echo "+ unzip"
-unzip 02-iosevka-term-2.3.2.zip
-echo "+ copy to system directory"
-cp ttf/*.ttf $sysdir
-echo "+ remove files"
-rm *.zip *.charmap *.css
-rm -r ttf ttf-unhinted woff woff2
+packages="pkg-iosevka pkg-iosevka-slab"
+for pkg in $packages; do
+	echo "+ download... $pkg"
+	wget https://github.com/be5invis/Iosevka/releases/download/v${VER}/${pkg}-${VER}.zip
+	echo "+ unzip"
+	unzip ${pkg}-${VER}.zip
+	echo "+ copy to system directory"
+	cp ttf/*.ttf $sysdir
+	echo "+ remove files"
+	rm *.zip *.css
+	rm -r ttf ttc ttf-unhinted woff woff2
+done
 echo "+ update font-cache"
 fc-cache
 echo "+ done"
