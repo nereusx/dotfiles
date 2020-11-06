6225cc59f1cc089ceca5f7c06eeaa8b2e35458ea
Nicholas Christopoulos <nereus@freemail.gr>
2020-11-06 15:10:27 +0200

    quick and dirty fix

--- etc/X11/xinit-rc/xde.bash
+++ etc/X11/xinit-rc/xde.bash
@@ -1,21 +1,21 @@
 # xde.database
 
-xde_count=17
+xde_count=19
 
-xde_max=16
+xde_max=18
 
 # table of keys
-xde_key=("xfce4" "plasma" "kde" "wmaker" "lxqt" "i3" "awesome" "e16" "fvwm2" "fvwm95" "openbox" "fluxbox" "blackbox" "mwm" "ctwm" "twm" "vtwm" )
+xde_key=("xfce4" "plasma" "kde" "wmaker" "lxqt" "i3" "awesome" "e16" "fvwm2" "fvwm95" "NsCDE" "openbox" "fluxbox" "blackbox" "wmfs" "mwm" "ctwm" "twm" "vtwm" )
 
 # table of descriptions
-xde_desc=("The Cholesterol Free Desktop Environment" "KDE: K Desktop Environment (plasma-x11)" "KDE: K Desktop Environment (standard)" "X11 Window Manager with a NEXTSTEP look and feel" "The lightweight Qt Desktop Environment" "Improved Tiling Window Manager" "Highly Configurable, Tiling Window Manager" "Enlightenment DR16 Window Manager" "An extremely powerful ICCCM-compliant window manager" "Fvwm2 with a Windows 95 look and feel" "Standards compliant, fast, light, extensible window manager" "Highly configurable and low resource X11 window manager" "Stacking window manager for X11" "Motif Window Manager" "Claude's Tab Window Manager" "Tab Window Manager for the X Window system" "Virtual Tab Window Manager" )
+xde_desc=("The Cholesterol Free Desktop Environment" "KDE: K Desktop Environment (plasma-x11)" "KDE: K Desktop Environment (standard)" "X11 Window Manager with a NEXTSTEP look and feel" "The lightweight Qt Desktop Environment" "Improved Tiling Window Manager" "Highly Configurable, Tiling Window Manager" "Enlightenment DR16 Window Manager" "An extremely powerful ICCCM-compliant window manager" "Fvwm2 with a Windows 95 look and feel" "Not So Common Desktop Environment" "Standards compliant, fast, light, extensible window manager" "Highly configurable and low resource X11 window manager" "Stacking window manager for X11" "Window Manager From Scratch" "Motif Window Manager" "Claude's Tab Window Manager" "Tab Window Manager for the X Window system" "Virtual Tab Window Manager" )
 
 # table of commands
-xde_cmd=("startxfce4" "startplasma-x11" "startkde" "wmaker" "lxqt" "i3" "awesome" "starte16" "fvwm2" "fvwm95" "openbox" "startflubox" "startblackbox" "mwm" "ctwm" "twm" "vtwm" )
+xde_cmd=("startxfce4" "startplasma-x11" "startkde" "wmaker" "lxqt" "i3" "awesome" "starte16" "fvwm2" "fvwm95" "nscde" "openbox" "startflubox" "startblackbox" "wmfs" "mwm" "ctwm" "twm" "vtwm" )
 
 # table of session execution prefix
-xde_spre=("exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session dbus-launch --exit-with-session start-lxqt" "exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session dbus-launch --exit-with-session" "start-fvwm2" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" )
+xde_spre=("exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session dbus-launch --exit-with-session start-lxqt" "exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session dbus-launch --exit-with-session" "exec ck-launch-session dbus-launch --exit-with-session" "start-fvwm2" "exec ck-launch-session" "exec ck-launch-session start-xwm" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" "exec ck-launch-session start-xwm" )
 
 # table of normal execution prefix
-xde_npre=("exec dbus-launch --exit-with-session" "exec" "exec" "exec dbus-launch --exit-with-session" "exec start-lxqt" "exec" "exec" "exec" "start-fvwm2" "exec" "exec" "exec" "exec" "exec" "exec" "exec" "exec" )
+xde_npre=("exec dbus-launch --exit-with-session" "exec" "exec" "exec dbus-launch --exit-with-session" "exec start-lxqt" "exec" "exec" "exec" "start-fvwm2" "exec" "exec" "exec" "exec" "exec" "exec" "exec" "exec" "exec" "exec" )
 
