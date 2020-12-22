cca37c36468d5a3b3f4540c3a2451ca9869338f5
Nicholas Christopoulos <nereus@freemail.gr>
2020-12-22 17:42:01 +0200

    tmux start session

--- .tmux/start-tmux.sh
+++ .tmux/start-tmux.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+# session name
+S="AN"
+case "$(hostname)" in
+quicksand64)	S="QS";;
+srv-x230)		S="SV";;
+void-t440s)		S="T4";;
+esac
+
+# create default layout
+tmux new-session -d -s $S 
 tmux new-window -n "root" 'sudo su -c "cd ; $SHELL"'
 tmux new-window -n "root" 'sudo su -c "cd ; $SHELL"'
 #tmux new-window -n "root" 'sudo su -c "cd ; $SHELL"'
@@ -8,5 +19,5 @@ tmux new-window -n "$USER"
 tmux new-window -n "last"
 tmux move-window -k -s 1 -t "last"
 tmux move-window -r
-
+tmux -2 attach-session -t $S
 
