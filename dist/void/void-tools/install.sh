9d128e119c6c2260ea2e99c4e1356ec7ab963663
Nicholas Christopoulos <nereus@freemail.gr>
2021-07-04 07:50:49 +0300

    quick and dirty fix

--- dist/void/void-tools/install.sh
+++ dist/void/void-tools/install.sh
@@ -10,7 +10,7 @@ for f in *; do
 	if [ ! -d $f ]; then
 		bas=${f%.*}
 		ext=${f#"$bas"}
-		if [ -z "$ext" -a -x "$bas" ]; then
+		if [ -z "$ext" ]; then
 			echo $f
 			install -m 755 -o root -g root "$f" "$dest/$f"
 		fi
